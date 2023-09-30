13a6ff82e86ba317e2039d1f20beb099c2d52d40
Linnéa Netzell <linnea.netzell@lindex.com>
2023-09-30 09:55:38 +0200

    Add new menu item.

---
+++ SamboPoints/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace SamboPoints.Models
+{
+    public class TodoItem
+    {
+        public string Todo { get; set; } = string.Empty;
+        public DateTime? DateAndTime { get; set; } = DateTime.Now;
+    }
+}

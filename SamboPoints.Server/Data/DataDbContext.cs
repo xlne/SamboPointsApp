7389802a64b24a372366d41752c37cc00465a4f4
Linnéa Netzell <linnea.netzell@lindex.com>
2024-01-23 13:48:55 +0100

    Adds server project

---
+++ SamboPoints.Server/Data/DataDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamboPoints.Server.Models;
+
+namespace SamboPoints.Server.Data;
+
+public class DataDbContext : DbContext
+{
+    public DbSet<TodoItem> TodoItems { get; set; }
+    public string DbPath { get; }
+
+    public DataDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "samboPoints.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    => options.UseSqlite($"Data Source={DbPath}");
+}

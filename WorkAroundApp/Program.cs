99934088cc02c3447dee2e32bfeedfb08b7492c6
Linnéa Netzell <linnea.netzell@lindex.com>
2024-02-20 16:44:28 +0100

    Adds work around for possibility to add migrations

---
+++ WorkAroundApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using SamboPoints.Server.Data;
+
+Console.WriteLine("Hello, World!");
+
+public class ContextFactory : IDesignTimeDbContextFactory<DataDbContext>
+{
+    public DataDbContext CreateDbContext(string[] args)
+        => new(new DbContextOptionsBuilder<DataDbContext>().UseSqlite(args[0]).Options);
+}
+
+/*
+ This project is created as a workaround due to a problem when creating migrations from my server project. Maybe a 
+common problem when working with Blazor WASM and SQLite, I have not investigated other frameworks. 
+
+Solution/workaround found here:
+https://github.com/dotnet/EntityFramework.Docs/issues/4230
+ */

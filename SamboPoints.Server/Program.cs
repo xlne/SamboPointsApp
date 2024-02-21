f5d8e99894fda06ff1eba37eb5f3aece69f91650
Linnéa Netzell <linnea.netzell@lindex.com>
2024-02-21 14:35:59 +0100

    Add DI for HttpClient and config for database

--- SamboPoints.Server/Program.cs
+++ SamboPoints.Server/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 using SamboPoints.Server;
+using SamboPoints.Server.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddDbContext<DataDbContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("SamboPointsDatabase")));
 
 await builder.Build().RunAsync();
